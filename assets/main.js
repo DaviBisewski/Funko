e4d7ff7784cf37b864d76fdf7c09b5d720cd8686
HenriqueChepli <henriquechepli@gmail.com>
2025-05-12 14:54:01 -0300

    feat: inclusão do pwa ao sistema

--- assets/main.js
+++ assets/main.js
@@ -163,6 +163,19 @@ createApp({
 
     const Funko = ref(funkos[0]);
 
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+          .then(registration => {
+            console.log('Service Worker registrado com sucesso:', registration);
+          })
+          .catch(error => {
+            console.log('Falha ao registrar o Service Worker:', error);
+          });
+      });
+    }
+    
+
     onMounted(() => {
       $(".carousel").slick({
         slidesToShow: 4, // diminui de 5 para 4, fica mais bonito em desktop
